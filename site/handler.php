bceef588b5a20bf84aa26a0f14eed27cc86c346a
Ubuntu <ubuntu@ec2-54-245-8-233.us-west-2.compute.amazonaws.com>
2014-04-24 19:36:15 +0000

    Site now can run executable

--- site/handler.php
+++ site/handler.php
@@ -1,40 +1,12 @@
 <?php 
-$executable = "btree";
-$result = array("action"=>array("success"=>"Invalid command"));
-$instruction = (isset($_POST['str'])) ? $_POST['str'] : null;
-$instrArr = ($instruction) ? preg_split('#\s+#', $instruction, null, PREG_SPLIT_NO_EMPTY) : null;
-$codes = array("new", "run", "insert", "delete", "select", "quit");
-// echo $instrArr[0];
-$instrCode = (array_search($instrArr[0], $codes)) ? $instrArr[0] : false;
-$instrArg = ($instrCode!="quit" && $instrCode) ? $instrArr[1] : "";
-$action = (isset($_POST['action'])) ? $_POST['action'] : null;
-if ($action=="start"){
-	//$res = exec("./"+$executable+" 60000");
-	$res = true;
-	$result["action"]["success"] = $res;
-}else if($instrCode!=false){
-	// try {
-	// 	$res = exec($instrCode+" "+$instrArg);
-	// } catch (Exception $e) {
-		$res = ($instrCode." ".strval($instrArg));
-	// }
-	$result["action"]["success"] = $res;
-	// try {
-	// 	$res = exec("getJson");
-	// } catch (Exception $e) {
-		$res = '[
-	        {
-	          "keys": ["32", "64", "128"], 
-	          "children": [
-	            {
-	              "keys": ["16", "24", "30"],
-	              "children": []
-	            }
-	          ]
-	        }
-	      ]';
-	// }
-	$result["action"]["result"] = json_decode($res);
+$file = "/var/www/btree/site/webInstr";
+$instructions = (file_get_contents("php://input"))? file_get_contents("php://input"):"quit" ;
+if (function_exists("file_put_contents")){
+	$write_file = (file_put_contents($file, $instructions)==false)? "error": "all good";
+}else{
+	$write_file = "Function not available";
 }
-echo json_encode($result);
+exec ("./btree webInstr jsonRes");
+$res = file_get_contents("jsonRes");
+echo $res;
 ?>
