ef9031394dc14cba698da599191ceb10d6e24649
Ubuntu <ubuntu@ec2-54-245-8-233.us-west-2.compute.amazonaws.com>
2014-04-24 00:28:35 +0000

    Started insert function and added second demo tree

--- program/btree.cpp
+++ program/btree.cpp
@@ -2,14 +2,90 @@
 
 btree::btree(int treeOrd){
 	order = treeOrd;
+	root = new node();
 }
 
 bool btree::deleteNode(int num){
 
 }
 
-bool btree::insert(int num){
+void btree::split(node *loc, int num){
+	//Find median
+	for (vector<int>::iterator it = loc->keys.begin(); it != loc->keys.end(); it++){ 
+		if ((*it) > num){
+			loc->keys.insert(it, num);
+			break;
+		}
+	}
+	int medianLoc = loc->keys.size()/2;
+	int median = loc->keys[medianLoc];
+	//Split based on median
+	node *left = new node();
+	node *right = new node();
+	for (vector<int>::iterator it = loc->keys.begin();it != loc->keys.end();it++){
+		if (*it < median){
+			left->keys.push_back(*it);
+		}else if(*it > median){
+			right->keys.push_back(*it);
+		}
+	}
+	loc->pointers.push_back(left);
+	loc->pointers.push_back(right);
+	loc->keys.clear();
+	loc->keys.push_back(median);
+	display();
+}
 
+void btree::display(){
+}
+
+bool btree::insert(int num){
+	node *loc = traverse(root, num);
+	//Nothing in tree
+	if (loc->keys.size()==0){
+		cout << "Node is empty procedure"<<endl;
+		loc->keys.push_back(num);
+		return true;
+	//Leaf node is not full
+	}else if (loc->keys.size() < order){
+		cout << "Node is not full procedure"<<endl;
+		bool inserted = false;
+		for (vector<int>::iterator it = loc->keys.begin(); it != loc->keys.end(); it++){ 
+			if ((*it) > num){
+				inserted = true;
+				loc->keys.insert(it, num);
+				break;
+			}else if ((*it) == num){
+				cout << "Already in tree"<<endl;
+				return false;
+			}
+		}
+		if (!inserted) loc->keys.push_back(num);
+		for (vector<int>::iterator it = loc->keys.begin();it!= loc->keys.end();it++) cout << *it << " ";
+		cout <<endl;
+		return true;
+	//Leaf node is full
+	}else{
+		cout << "Node is full procedure"<<endl;
+		split(loc, num);
+		return true;		
+	}
+}
+
+node *btree::traverse(node *cur, int num){
+	//If the node has no children
+	if (cur->pointers.size() == 0) return cur; 
+	int count = 0;
+	for (vector<int>::iterator it = cur->keys.begin(); it != cur->keys.end(); it++){
+		if (num < *it){
+			//Search further into the tree
+			return traverse(cur->pointers[count], num);
+		}else if (num == (*it)){
+			//Key is in this node
+			return cur;
+		}
+		count ++;
+	}
 }
 
 bool btree::select(int num){
@@ -21,5 +97,5 @@ string btree::getJson(){
 } 
 
 btree::~btree(){
-	delete root;
+	
 }
--- program/btree.h
+++ program/btree.h
@@ -6,7 +6,7 @@
 using namespace std;
 
 struct node{
-	vector<int> limits;
+	vector<int> keys;
 	vector<struct node*> pointers;
 };
 
@@ -15,10 +15,13 @@ class btree{
 		int order;
 		node *root;
 		string json;
+		void split(node *loc, int num);
+		node *traverse(node *cur, int num);
 	public:
 		btree(int treeOrd);
 		~btree();
 		string getJson();
+		void display();
 		bool insert(int num);
 		bool deleteNode(int num);
 		bool select(int num);
