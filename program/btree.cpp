44449a225927fa4480a0c486fd10075efc58a4b4
Ubuntu <ubuntu@ec2-54-245-8-233.us-west-2.compute.amazonaws.com>
2014-04-25 20:01:52 +0000

    Insert all working...I think

--- program/btree.cpp
+++ program/btree.cpp
@@ -12,7 +12,6 @@ bool btree::deleteNode(int num){
 
 void btree::split(node *loc, int num, vector<struct node*> leftData, vector<struct node*> rightData){
 	//Find median after adding new key to the mix
-	cout << "Finding median"<<endl;
 	bool inserted = false;
 	for (vector<int>::iterator it = loc->keys.begin(); it != loc->keys.end(); it++){ 
 		if ((*it) > num){
@@ -25,11 +24,12 @@ void btree::split(node *loc, int num, vector<struct node*> leftData, vector<stru
 	int medianLoc = loc->keys.size()/2;
 	int median = loc->keys[medianLoc];
 	//Split based on median
-	cout << "Splitting based on median"<<endl;
 	node *left = new node();
 	node *right = new node();
 	left->parent = loc;
 	right->parent = loc;
+	loc->pointers.push_back(left);
+	loc->pointers.push_back(right);
 	for (vector<int>::iterator it = loc->keys.begin();it != loc->keys.end();it++){
 		if (*it < median){
 			left->keys.push_back(*it);
@@ -37,38 +37,66 @@ void btree::split(node *loc, int num, vector<struct node*> leftData, vector<stru
 			right->keys.push_back(*it);
 		}
 	}
-	//Solution: Deep Copy so the pointers arent deleted
-	for (vector<struct node*>::iterator it = leftData.begin(); it!=leftData.end();it++) left->pointers.push_back(*it);
-	for (vector<struct node*>::iterator it = rightData.begin(); it!=rightData.end();it++) right->pointers.push_back(*it);
+	loc->keys.clear();
+	loc->keys.push_back(median);
+	right->pointers = rightData;
+	left->pointers = leftData;
+	//Update the parent's of the children nodes
+	for (vector<struct node*>::iterator it = right->pointers.begin();it!=right->pointers.end();it++) (*it)->parent = right;
+	for (vector<struct node*>::iterator it = left->pointers.begin();it!=left->pointers.end();it++) (*it)->parent = left;
+
 	//Push back up so higher nodes are filled
 	if (loc->parent == NULL){
-		cout << "create new root"<<endl;
 		node *temp = new node();
 		temp->parent = NULL;
 		root->parent = temp;
 		root = temp;		
 	}
-	cout << "Starting recursive split"<<endl;
 	if (loc->parent->keys.size() < order){
-		loc->parent->keys.push_back(median);
+		cout << "Median insert start"<<endl;
+		bool inserted = false;
+		for (vector<int>::iterator it = loc->parent->keys.begin();it!=loc->parent->keys.end();it++){
+			if ((*it) > median){
+				loc->parent->keys.insert(it, median);
+				inserted  = true;
+				break;
+			}
+		}
+		if (!inserted) loc->parent->keys.push_back(median);
 		left->parent = loc->parent;
 		right->parent = loc->parent;
-		loc->parent->pointers.push_back(left);
-		loc->parent->pointers.push_back(right);
+		inserted = false;
+		cout << "Parent pointers insert start"<<endl;
+		for (vector<struct node*>::iterator it = loc->parent->pointers.begin(); it!=loc->parent->pointers.end();it++){
+			if ((*it)->keys.size() > 0 && (*it)->keys[0] > left->keys[0]){
+				it = loc->parent->pointers.insert(it, right);
+				loc->parent->pointers.insert(it, left);
+				inserted = true;
+				break;
+			}
+		}
+		if (!inserted){
+			loc->parent->pointers.push_back(left);
+			loc->parent->pointers.push_back(right);
+		}
+		cout << "end"<<endl;
 		for (vector<struct node*>::iterator it = loc->parent->pointers.begin(); it!=loc->parent->pointers.end();){  
 			if ((*it)==loc){
 				loc->parent->pointers.erase(it);
-			}else ++it;
+			}else{
+				++it;
+			}
 		}
 		delete loc;
+		cout << "99 Problems"<<endl;
 	}else{
-	
 		for (vector<struct node*>::iterator it = loc->parent->pointers.begin(); it!=loc->parent->pointers.end();){  
 			if ((*it)==loc){
 				loc->parent->pointers.erase(it);
-			}else ++it;
+			}else{
+				++it;
+			}
 		}
-		
 		if (loc->parent->keys[0] < loc->keys[0]){
 			split(loc->parent, median, loc->parent->pointers, loc->pointers);
 		}else{
@@ -79,17 +107,15 @@ void btree::split(node *loc, int num, vector<struct node*> leftData, vector<stru
 	}
 }
 
-
 bool btree::insert(int num){
 	node *loc = traverse(root, num);
 	//Nothing in tree
 	if (loc->keys.size()==0){
-		cout << "Node is empty procedure"<<endl;
 		loc->keys.push_back(num);
 		return true;
 	//Leaf node is not full
 	}else if (loc->keys.size() < order){
-		cout << "Node is not full procedure"<<endl;
+		cout << "split not required"<<endl;
 		bool inserted = false;
 		for (vector<int>::iterator it = loc->keys.begin(); it != loc->keys.end(); it++){ 
 			if ((*it) > num){
@@ -107,7 +133,7 @@ bool btree::insert(int num){
 		return true;
 	//Leaf node is full
 	}else{
-		cout << "Node is full procedure"<<endl;
+		cout << "Split required"<<endl;
 		vector<struct node*> temp;
 		split(loc, num, temp, temp);
 		return true;		
@@ -147,13 +173,14 @@ string btree::jsonAux(node *cur){
 	ss << "{";
 	ss << "\"keys\": [";
 	int count = 0;
+	if (cur->parent != NULL) cout << "Parent: "<<cur->parent->keys[0]<<", keys: ";
 	for (vector<int>::iterator it = cur->keys.begin(); it!= cur->keys.end() && count <= order;it++){
+		cout << (*it)<<" ";
 		ss << "\"" << *it << "\"";
-		cout << (*it)<<", ";
 		count ++;
 		if (*it!=cur->keys.back()) ss << ", ";
 	}
-	cout <<endl;
+	cout << endl;
 	ss << "], \"children\": [";
 	for (vector<struct node*>::iterator it = cur->pointers.begin(); it!=cur->pointers.end();it++){
 		ss << jsonAux(*it);
---
+++ program/moveExe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp btree /var/www/btree/site
+sudo chmod 777 /var/www/btree/site/btree
