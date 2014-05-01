8fe0d087d5f2ad7d2fa0a165ac1c642c204670ea
DRab7792 <dan.a.rabinowitz@gmail.com>
2014-05-01 15:50:57 -0400

    FInished website, wrote readme files

--- program/README.md
+++ program/README.md
@@ -2,3 +2,14 @@ btree
 =====
 
 C++ implementation of a B Tree
+
+In order to compile this program type 'make all' into the command line.
+To run the program type './btree'
+There are several commands to run once you start the program:
+new <order> --This creates a new tree of whatever order you specify, if a tree exists it will be deleted
+insert <num> --This inserts a new key into the tree
+select <num> --This searches the tree for a key, marking all nodes along the way
+delete <num> --This deletes a key from the tree
+getjson --This will get the JSON info for the tree
+run <file> --This will run a batch file of these commands making it easy to save trees
+quit --Quits the program
--- program/btree.h
+++ program/btree.h
@@ -8,17 +8,17 @@
 using namespace std;
 
 struct node{
-	vector<int> keys;
-	vector<struct node*> pointers;
-	node *parent;
-	bool selected;
+	vector<int> keys; //All the keys of this node in order
+	vector<struct node*> pointers; //All the pointers of this node in order
+	node *parent; //Pointer to the parent, if root then parent is NULL
+	bool selected; //Marks this node as selected if the last command was a select command
 };
 
 class btree{
 	private:
-		int order;
-		int boundary;
-		node *root;
+		int order; //Order of the tree
+		int boundary; //Underflow Boundary
+		node *root; //Root node
 		string jsonAux(node *cur);
 		void split(node *loc, int num, vector<struct node*> data);
 		void unMarkNodes(node *cur);
